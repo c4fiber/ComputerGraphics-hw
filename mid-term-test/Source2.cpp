22bd8f6c1841a671a4022f8b7d1e2f7457848b4b
Richard Shin <qudcjf153@gmail.com>
2020-10-22 12:25:37 +0900

    mouse rotate test

---
+++ mid-term-test/Source2.cpp
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include "GL/glut.h"
+
+void mouseClick(int, int, int, int);
+void init();
+void display();
+void teapot();
+
+void mouseClick(int button, int state, int x, int y) {
+    if (state == GLUT_DOWN) {
+        rand() % 255
+    }
+}
+
+void teapot() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -7.0f);
+
+	glColor3f(0.0, 1.0, 0.0);
+	glutSolidTeapot(2.0);
+	glPopMatrix();
+}
+
+void init() {
+
+    // Set the current clear color to sky blue and the current drawing color to
+    // white.
+    glClearColor(0.1, 0.39, 0.88, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
+
+    // Tell the rendering engine not to draw backfaces.  Without this code,
+    // all four faces of the tetrahedron would be drawn and it is possible
+    // that faces farther away could be drawn after nearer to the viewer.
+    // Since there is only one closed polyhedron in the whole scene,
+    // eliminating the drawing of backfaces gives us the realism we need.
+    // THIS DOES NOT WORK IN GENERAL.
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    // Set the camera lens so that we have a perspective viewing volume whose
+    // horizontal bounds at the near clipping plane are -2..2 and vertical
+    // bounds are -1.5..1.5.  The near clipping plane is 1 unit from the camera
+    // and the far clipping plane is 40 units away.
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-2, 2, -1.5, 1.5, 1, 40);
+
+    // Set up transforms so that the tetrahedron which is defined right at
+    // the origin will be rotated and moved into the view volume.  First we
+    // rotate 70 degrees around y so we can see a lot of the left side.
+    // Then we rotate 50 degrees around x to "drop" the top of the pyramid
+    // down a bit.  Then we move the object back 3 units "into the screen".
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0, 0, -3);
+    glRotatef(50, 1, 0, 0);
+    glRotatef(70, 0, 1, 0);
+}
+
+// Initializes GLUT, the display mode, and main window; registers callbacks;
+// does application initialization; enters the main event loop.
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("test bcs");
+
+    glutDisplayFunc(display);
+    init();
+    glutMainLoop();
+}
